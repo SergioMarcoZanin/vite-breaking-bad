b6e9047cb1b7aaccdaae872c738f2e68340eb77b
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-11-28 16:23:01 +0100

    creazione file

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header class="d-flex align-items-center p-2">
+    <img src="../assets/logo.png" alt="Logo Breaking Bad" />
+    <h1>Breaking Bad Api</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  color: white;
+  img {
+    max-width: 6.25rem;
+    height: 1.875rem;
+  }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "AppMain",
+};
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
