f4aaae037af00b58caf938a4e00ae9d04d853264
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-11-29 15:47:50 +0100

    inserimento cards

---
+++ src/components/AppLoading.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppLoading",
+};
+</script>
+
+<template>
+  <div class="text-center">
+    <div class="spinner-border my-5" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "CharacterCard",
+  props: {
+    info: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="p-3 m-2 text-center">
+    <img class="img-fluid" :src="info.img" :alt="info.name" />
+    <h5 class="fs-6 fw-bold mt-4">{{ info.name }}</h5>
+    <div class="series mt-3">{{ info.category }}</div>
+    <div class="status mb-5">{{ info.status }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.series,
+.status {
+  color: #6b6a6c;
+}
+</style>
